50b91696561584fd43b9167dd1e7b1f8d04e731e
zhangzhongguo <zhangzhongguo@58.com>
2021-07-17 11:02:30 +0800

    feat

---
+++ netty-test/src/main/java/com/tany/TestNetty.java
@@ -0,0 +1,19 @@
+package com.tany;
+
+/**
+ * @author zhangzhongguo
+ * @date 2021/07/17
+ * 测试netty
+ */
+public class TestNetty {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+
+    }
+
+
+}
